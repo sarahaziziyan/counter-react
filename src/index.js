2425c632829de4ad640921130b4ecd9728de6693
Sarah Aziziyan <aziziyan.sarah@yahoo.com>
2019-09-26 15:54:42 +0330

    added a component (counter.jsx), made a hello world with that component, added Bootstrap to the project.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
 
 
-const element = <h1>Hello World!</h1>
-console.log(element);
-ReactDOM.render(element,document.getElementById('root'));
+ReactDOM.render(<Counter/>,document.getElementById("root"));
