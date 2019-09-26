597feb46dc053d4010fd57c36a56324a9900f746
Sarah Aziziyan <aziziyan.sarah@yahoo.com>
2019-09-26 16:23:58 +0330

    added a formatCount method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 1
     }
     render() {
         return (
             <React.Fragment>
-                <span>{this.state.count}</span>
+                <span>{this.formatCount()}</span>
                 <button>Increment</button>
             </React.Fragment>
         );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
 
