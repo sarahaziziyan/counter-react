af349215cb7127df7b5919e64ae1b9115c6b209b
Sarah Aziziyan <aziziyan.sarah@yahoo.com>
2019-09-29 23:37:27 +0330

    typo fix

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
     render() {
         return (
             <React.Fragment>
-                <NavBar tottalCounters={this.state.counters.filter(c => c.value > 0).length} />
+                <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
                 <main className="container">
                     <Counters
                         counters={this.state.counters}
