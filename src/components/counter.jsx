9f3c017799246fc742c0cfb0f6e76f6798e19126
Sarah Aziziyan <aziziyan.sarah@yahoo.com>
2019-09-30 23:23:41 +0330

    using ajax with Fetch

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     componentDidUpdate(prevProps, prevState) {
-        console.log("prevProp", prevProps);
-        console.log("prevState", prevState);
         if (prevProps.counter.value !== this.props.counter.value) {
-            // Ajax Call
+
+            fetch("/myurl")
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        this.setState({
+                            isLoaded: true,
+                            items: result.items
+                        });
+                    },
+                    // Note: it's important to handle errors here
+                    // instead of a catch() block so that we don't swallow
+                    // exceptions from actual bugs in components.
+                    (error) => {
+                        this.setState({
+                            isLoaded: true,
+                            error
+                        });
+                    }
+                )
+
+
             console.log("prevProps value differs from props value");
         }
     }
